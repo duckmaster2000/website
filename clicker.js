e52c7642b6a9759e4eeef495e378dd71591f48fe
duckmaster2000 <31calebl@students.harker.org>
2025-10-04 20:53:00 -0700

    Update clicker.js

--- clicker.js
+++ clicker.js
@@ -1,5 +1,29 @@
 let gem = document.querySelector('.gem-cost');
+let parsedgem = parseFloat(gem.innerHTML);
+
+let clickerCost = document.querySelector('.clicker-cost')
+let parsedClickerCost = parseFloat(clickerCost.innerHTML);
+let clickerLevel = document.querySelector('.clickerLevel');
+let clickerIncrease = document.querySelector('.clickerIncrease');
+let parsedClickerIncrease = parseFloat(clickerIncrease.innerHTML);
+
+let gpc = 1;
 
 function incrementGem() {
-    gem.innerHTML = parseFloat(gem.innerHTML) + 1;
+    gem.innerHTML = Math.round(parsedgem += gpc);
+}
+
+function buyClicker() {
+    if (parsedgem >= parsedClickerCost) {
+        gem.innerHTML = Math.round(parsedgem -= parsedClickerCost);
+
+        clickerLevel.innerHTML++;
+
+        parsedClickerIncrease = parseFloat((parsedClickerIncrease * 1.03).toFixed(2));
+        clickerIncrease.innerHTML = parsedClickerIncrease;
+        gpc += parsedClickerIncrease;
+
+        parsedClickerCost *= 1.18;
+        clickerCost.innerHTML = Math.round(parsedClickerCost);
+    }
 }
